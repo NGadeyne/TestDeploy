a4e4c09e34c60b707483ef058d5e32abbcf1d116
NGadeyne <nicolas.g@thewalkingnerds.com>
2025-04-09 20:11:22 +0200

    test28

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from Flask on DigitalOcean App Platform 🎉'
+    return 'Hello from Flask on DigitalOcean App Platform 🎉 '
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
